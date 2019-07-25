791e2778b535904ae85c928da2d05e670dfb1b57
jaak <dev@globally.ltd>
2019-07-25 16:46:06 +0200

    Use markov model in backend api

--- api/app.py
+++ api/app.py
@@ -1,6 +1,18 @@
+import os
+
 from flask import Flask, jsonify
+
+from model.spacy_markov import SpacyMarkovify
+
+MARKOV_MODEL_PATH = os.getenv("MARKOV_MODEL_PATH")
+with open(MARKOV_MODEL_PATH) as fh:
+    model_json = fh.read()
+    model = SpacyMarkovify.from_json(model_json)
+
 app = Flask(__name__)
 
-@app.route('/laws')
+
+@app.route("/laws")
 def get_law():
-    return jsonify('Such a good law.')
+    law = model.make_short_sentence(280)
+    return jsonify(law)
