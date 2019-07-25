42556ddac95a371ad8ca1bdc35896a05dc310401
jaak <dev@globally.ltd>
2019-07-25 19:50:55 +0200

    Only useEffect on load

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,14 @@ import React, {useEffect, useState} from 'react';
 import "./App.css";
 import {Law} from "./components/Law";
 
-export const App = () => {
+function App() {
   const [law, setLaw] = useState([]);
   
   useEffect(() =>{
     fetch('/laws').then(response => response.json()).then(data => {
       setLaw(data);
     })
-  })
+  }, [])
 
  
   return (
